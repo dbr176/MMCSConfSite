2e019cc45671f87fab7d4b5ceaeb04b12f0da88c
Гончаров Алексей <darin100@yandex.com>
2016-12-08 21:06:48 +0300

    Добавил таблицы для докладов

--- Model.hs
+++ Model.hs
@@ -4,6 +4,8 @@ module Model where
 
 import ClassyPrelude.Yesod
 import Database.Persist.Quasi
+import Database.Persist.Sqlite
+import Data.Time
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
@@ -11,3 +13,4 @@ import Database.Persist.Quasi
 -- http://www.yesodweb.com/book/persistent/
 share [mkPersist sqlSettings, mkMigrate "migrateAll"]
     $(persistFileWith lowerCaseSettings "config/models")
+
