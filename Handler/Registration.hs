780cbf8833e67c926dc7eff30eb97f950dff8fcb
Naseka Andrew <duha.an.ru@yandex.ru>
2016-12-20 12:09:31 +0300

    Добавлены формы для ФИО, название доклада

--- Handler/Registration.hs
+++ Handler/Registration.hs
@@ -4,11 +4,31 @@ import Import
 import DbUtils
 import qualified System.IO as SIO
 import Text.Read
+import Data.Time
+import Data.List.Split
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
+import Database.Persist.Sql
+import Database.Persist
 
 getRegistrationR :: Handler Html
-getRegistrationR = do defaultLayout [whamlet|<h1>Page of registration|]
+getRegistrationR = do 
+	defaultLayout 
+		[whamlet|
+            <center>
+            <h1> Регистрация </h1> 
+                <div> 
+                    <label for="form_sname">Фамилия</label>
+                    <input type="Фамилия" id="form_sname" name="second_name"/> </div>
+                <div> 
+                    <label for="form_fname">Имя</label>                	
+                    <input type="Имя" id="form_fname" name="first_fname"/> </div>
+                <div> 
+                    <label for="form_tname">Отчество</label>
+                    <input type="Отчество" id="form_tname" name="third_name"/> </div>
+                <div> 
+                    <label for="form_titrep">Название доклада</label>                
+                    <input type="Название доклада" id="form_titrep" name="title_report"/> </div>
+        |]
 
 postRegistrationR :: Handler Html 
 postRegistrationR = do defaultLayout [whamlet|<h1>Page of registration|]
