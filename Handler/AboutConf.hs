1b4bc7588003ee4ac4776ff6dc8732523a3147ee
Alexander Birukov <sanya.rnd@gmail.com>
2016-12-21 21:06:41 +0300

    Добавлен прототип слайдов для фото

--- Handler/AboutConf.hs
+++ Handler/AboutConf.hs
@@ -18,7 +18,19 @@ getAboutConfR = do  defaultLayout [whamlet|<div .container>
         <h2>Карта проезда:</h2>
         <iframe src="https://api-maps.yandex.ru/frame/v1/-/CZH8b6Ny" width="560" height="400" frameborder="0"></iframe>
         <h2>Фотографии:</h2>
-        <p><img src="static/mech.jpg" width="80%" height="50%" alt="фасад мехмата"></p>
+        <ul id="nav">
+            <li><a href="#slide-1">Slide 1</a>
+            <li><a href="#slide-2">Slide 2</a>
+            <li><a href="#slide-3">Slide 3</a>
+            <li><a href="#slide-4">Slide 4</a>
+            <li><a href="#slide-5">Slide 5</a>
+        <div id="slides">
+            <section id="slide-1"><img src="static/mech.jpg" width="80%" height="50%" alt="фасад мехмата"></section>
+            <section id="slide-2"><img src="static/mech.jpg" width="80%" height="50%" alt="фасад мехмата"></section>
+            <section id="slide-3"><img src="static/mech.jpg" width="80%" height="50%" alt="фасад мехмата"></section>
+            <section id="slide-4">This is Slide 4</section>
+            <section id="slide-5">This is Slide 5</section>
+
     |]
 
 
