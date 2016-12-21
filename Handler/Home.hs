4e5ea1aed5379929408c2a324eb0e5b10f416efb
cubman <tolik.abramoff@yandex.ru>
2016-12-22 00:08:14 +0300

    добавил вывод спонсоров

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -3,10 +3,21 @@ module Handler.Home where
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
+import DbUtils
 
 -- Обработчик GET запроса
 getHomeR :: Handler Html
-getHomeR = do defaultLayout [whamlet|<div .container>
+getHomeR = do 
+        sponsors <- (runDB $ getSponsors)
+
+        defaultLayout [whamlet|<div .container>
+        
+        $forall (Sponsor n (Just r)) <- dropEntityList sponsors
+        <a href="" target="_blank">
+         <img src="static/mech.jpg"  alt="рыбка">
+        
+                   
+
         <a class="boxed" href="mailto:email@whatever.com?subject=Partnership">Стать партнером</a>
 
         <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
