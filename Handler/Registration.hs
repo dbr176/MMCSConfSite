200c1b346384ab2b1596a06f7558b47f747d9499
Alexander Birukov <sanya.rnd@gmail.com>
2016-12-21 18:30:50 +0300

    Заменены табы на пробелы

--- Handler/Registration.hs
+++ Handler/Registration.hs
@@ -11,41 +11,33 @@ import Database.Persist.Sql
 import Database.Persist
 
 data RegInform = RegInform {
-	firstName :: Text,
-	secondName :: Text,
-	thirdName :: Text,
-	report :: Text
+    firstName :: Text,
+    secondName :: Text,
+    thirdName :: Text,
+    report :: Text
 }
 
 
 getRegistrationR :: Handler Html
-getRegistrationR = do 
-	defaultLayout 
-		[whamlet|
-        <div .container>
-                            <div .bs-docs-section>
-                                <div .row>
-                                    <div .col-lg-12>
-                                        
-                                           
-            <h1> Регистрация </h1> 
-            <h4>
-               <div> 
-                    <label for="form_sname">Фамилия</label>
-                    <input type="Фамилия" id="form_sname" name="second_name"/> 
-                <div> 
-                    <label for="form_fname">Имя</label>                	
-                    <input type="Имя" id="form_fname" name="first_fname"/> 
-                <div> 
-                    <label for="form_tname">Отчество</label>
-                    <input type="Отчество" id="form_tname" name="third_name"/> 
-                <div> 
-                    <label for="form_titrep">Название доклада</label>                
-                    <input type="Название доклада" id="form_titrep" name="title_report"/>
-               <div>
-                  <a .btn.btn-info.btn-lg>
-                     Подать заявку
+getRegistrationR = do defaultLayout [whamlet|
+        <h1> Регистрация </h1>
+        <h4>
+           <div>
+                <label for="form_sname">Фамилия</label>
+                <input type="Фамилия" id="form_sname" name="second_name"/>
+            <div>
+                <label for="form_fname">Имя</label>
+                <input type="Имя" id="form_fname" name="first_fname"/>
+            <div>
+                <label for="form_tname">Отчество</label>
+                <input type="Отчество" id="form_tname" name="third_name"/>
+            <div>
+                <label for="form_titrep">Название доклада</label>
+                <input type="Название доклада" id="form_titrep" name="title_report"/>
+           <div>
+              <a .btn.btn-info.btn-lg>
+                 Подать заявку
         |]
 
-postRegistrationR :: Handler Html 
+postRegistrationR :: Handler Html
 postRegistrationR = do defaultLayout [whamlet|<h1>Page of registration|]
