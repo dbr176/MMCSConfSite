7b40b5699772b12a47805b20aa9f4e493c5536e6
Naseka Andrew <duha.an.ru@yandex.ru>
2016-12-21 21:50:33 +0300

    Исправлена структура RegInform

--- Handler/Registration.hs
+++ Handler/Registration.hs
@@ -12,10 +12,11 @@ import Database.Persist
 import Text.Julius (RawJS (..))
 
 data RegInform = RegInform {
-    name :: Text,
-    email :: Text,
-    username :: Text,
-    phone :: Text
+    firstName :: Text,
+    secondName :: Text,
+    thirdName :: Text,
+    report :: Text,
+    about :: Text
 } deriving(Show)
 
 -- TODO: Вставить данные в новую форму
@@ -49,22 +50,24 @@ getRegistrationR = do defaultLayout [whamlet|<div .container><form action=@{Regi
             <p class="contact"><label for="tname">Отчество</label></p>
             <input id="tname" name="third_name" placeholder="Введите отчество" required="" type="email">
 
-            <p class="contact"><label for="username">Название доклада</label></p>
-            <input id="username" name="username" placeholder="Введите тему доклада" required="" tabindex="2" type="text">
+            <p class="contact"><label for="treport">Название доклада</label></p>
+            <input id="treport" name="title_report" placeholder="Введите тему доклада" required="" tabindex="2" type="text">
 
-            <p class="contact"><label for="phone">О себе</label></p>
-            <textarea id="phone" name="phone" placeholder="Напишите о себе что-нибудь интересное" required="" type="text">
+            <p class="contact"><label for="_about_you">О себе</label></p>
+            <textarea id="_about_you" name="about_you" placeholder="Напишите о себе что-нибудь интересное" required="" type="text">
 
-            <input class="buttom" name="submit" id="submit" tabindex="5" value="Sign me up!" type="submit">
+            <input class="buttom" name="submit" id="submit" tabindex="5" value="Зарегистрироваться" type="submit">
     |]
 
 getInputR = do
     inf <- runInputGet $ RegInform
-                <$> ireq textField "name"
-                <*> ireq textField "email"
-                <*> ireq textField "username"
-                <*> ireq textField "phone"
-    addReportRequest (name $ inf) (email $ inf) (username $ inf)
+                <$> ireq textField "first_name"
+                <*> ireq textField "second_name"
+                <*> ireq textField "third_name"
+                <*> ireq textField "title_report"
+                <*> ireq textField "about_you"
+    addReportRequest (report $ inf) 
+    	(secondName inf ++ firstName inf ++ thirdName inf) (about $ inf)
 
 postRegistrationR :: Handler Html
 postRegistrationR = undefined
