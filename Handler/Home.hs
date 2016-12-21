19abb43bdbe9f010a2dc65716e46bd4f1b092cab
Alexander Birukov <sanya.rnd@gmail.com>
2016-12-21 19:08:10 +0300

    Добавлена кнопка "стать партнером"

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -6,9 +6,10 @@ import Text.Julius (RawJS (..))
 
 -- Обработчик GET запроса
 getHomeR :: Handler Html
-getHomeR = do defaultLayout [whamlet|
-        <center>
-            <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
-            <script src="//yastatic.net/share2/share.js"></script>
-            <div class="ya-share2" data-services="collections,vkontakte,facebook,odnoklassniki,moimir" data-counter=""></div>
-        |]
+getHomeR = do defaultLayout [whamlet|<div .container>
+        <a class="boxed" href="mailto:email@whatever.com?subject=Partnership">Стать партнером</a>
+
+        <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+        <script src="//yastatic.net/share2/share.js"></script>
+        <div class="ya-share2" data-services="collections,vkontakte,facebook,odnoklassniki,moimir" data-counter=""></div>
+    |]
