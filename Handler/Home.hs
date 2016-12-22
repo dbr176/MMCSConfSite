c8829f4d6ce51edab0a0836815058151a038bedd
cubman <tolik.abramoff@yandex.ru>
2016-12-22 14:43:04 +0300

    Добавил вывод участников конференции

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,11 +4,16 @@ import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
 import DbUtils
+import Database.Persist.Sql as SQL
 
 -- Обработчик GET запроса
 getHomeR :: Handler Html
 getHomeR = do 
-        sponsors <- (runDB $ getSponsors)
+        sponsors <- runDB $ getSponsors
+        members <- runDB $ getUsersInfo
+        let ms =  dropEntityList members
+        rms <- sequence $ map (\(UserInfo r _) -> runDB $ SQL.get r) ms
+        let msr = zipWith (,) ms rms
 
         defaultLayout [whamlet|
         <div .masthead>
@@ -23,7 +28,7 @@ getHomeR = do
             $forall (Sponsor n (Just r)) <- dropEntityList sponsors   
                 <div class="thumbs">
                     <td> <a href= "#{r}" target="_blank">
-                        <img src="static/sponsor/#{n}.jpg"/></a>  </td>
+                        <img src="static/sponsor/#{n}.jpg"/></a></td>
                         <div class="caption">
                             <span class="title">#{n}</span>   
                             <p>
@@ -32,7 +37,13 @@ getHomeR = do
          <a class="boxed" href="mailto:email@whatever.com?subject=Partnership">Стать партнером</a>
 
          <h1> Участники мероприятия</h1>
-
+            $forall ((UserInfo _ _) , Just (User idn _)) <- msr  
+                <div class="thumbs">
+                    <td><img src="static/members/#{idn}.jpg"/></td>
+                        <div class="caption">
+                            <span class="title">#{idn}</span>   
+                            <p>
+                            <p> 
         
 
         <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
