87f26fd4f510f11a07d26371abaa869786be5617
cubman <tolik.abramoff@yandex.ru>
2016-12-22 13:10:34 +0300

    Визульные изменения главной страницы

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -10,20 +10,30 @@ getHomeR :: Handler Html
 getHomeR = do 
         sponsors <- (runDB $ getSponsors)
 
-        defaultLayout [whamlet|<div .container>
-      
-       <h1> Спонсоры мероприятия </h1>
-        $forall (Sponsor n (Just r)) <- dropEntityList sponsors   
-            <div class="thumbs">
-               <td> <a href= "#{r}" target="_blank">
-                <img src="static/sponsor/#{n}.jpg"/></a>  </td>
-                <div class="caption">
-                    <span class="title">#{n}</span>   
-                    <p>   
+        defaultLayout [whamlet|
+        <div .masthead>
+            <div .container>
+                 <h1>
+                    Добро пожаловать на конференцию 2 0 1 6
+                 <a href="http://localhost:3000/aboutconf/" .btn.btn-info.btn-lg>
+                        Условие конференции
 
-       <h1> Участники мероприятия</h1>
+       <div .container>
+        <h1> Спонсоры мероприятия </h1>
+            $forall (Sponsor n (Just r)) <- dropEntityList sponsors   
+                <div class="thumbs">
+                    <td> <a href= "#{r}" target="_blank">
+                        <img src="static/sponsor/#{n}.jpg"/></a>  </td>
+                        <div class="caption">
+                            <span class="title">#{n}</span>   
+                            <p>
+                            <p> 
 
-        <a class="boxed" href="mailto:email@whatever.com?subject=Partnership">Стать партнером</a>
+         <a class="boxed" href="mailto:email@whatever.com?subject=Partnership">Стать партнером</a>
+
+         <h1> Участники мероприятия</h1>
+
+        
 
         <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
         <script src="//yastatic.net/share2/share.js"></script>
